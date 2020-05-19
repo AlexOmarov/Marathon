69380f74391ec547a6693e0142209d6f810cd551
AlexOmarov <shtil.a@yandex.ru>
2020-05-19 21:57:21 +0700

    Added fragment view model data binding support

--- app/src/main/java/ru/somarov/marathon/ui/main/plugin/runner_card/MainFragment.kt
+++ app/src/main/java/ru/somarov/marathon/ui/main/plugin/runner_card/MainFragment.kt
@@ -1,14 +1,14 @@
 package ru.somarov.marathon.ui.main.plugin.runner_card
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
-import kotlinx.android.synthetic.main.main_fragment.*
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import ru.somarov.marathon.R
+import ru.somarov.marathon.databinding.MainFragmentBinding
 import ru.somarov.marathon.ui.main.plugin.runner_card.model.MainViewModel
 
 class MainFragment : Fragment() {
@@ -22,15 +22,16 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        val binding: MainFragmentBinding = DataBindingUtil.inflate(
+            inflater, R.layout.main_fragment, container, false)
+        binding.userViewModel = viewModel
+        binding.lifecycleOwner = this
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        viewModel.user.observe(viewLifecycleOwner, Observer {
-            message.text = it
-        })
     }
 
 }
--- app/src/main/java/ru/somarov/marathon/ui/main/plugin/runner_card/model/MainViewModel.kt
+++ app/src/main/java/ru/somarov/marathon/ui/main/plugin/runner_card/model/MainViewModel.kt
@@ -6,9 +6,15 @@ import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     private val _user = MutableLiveData<String>()
+    private val secondName = "The most beautiful man on Earth"
+    private val firstName = "John"
     val user: LiveData<String>
         get() = _user
     init {
-        _user.value = "John"
+        _user.value = firstName
+    }
+
+    fun changeName() {
+        if (secondName == _user.value) _user.value = firstName else _user.value = secondName
     }
 }
