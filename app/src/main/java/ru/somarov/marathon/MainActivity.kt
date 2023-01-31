2f92c0edc943a7bdde1d78d03e81c9ad4ffccd83
Alex Omarov <shtil.a@yandex.ru>
2023-01-31 11:56:03 +0700

    Restore project with newer dependencies

--- app/src/main/java/ru/somarov/marathon/MainActivity.kt
+++ app/src/main/java/ru/somarov/marathon/MainActivity.kt
@@ -12,9 +12,6 @@ import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
-    // Reference to the application graph that is used across the whole app
-    // val appComponent = DaggerApplicationComponent.create()
-
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,14 +25,6 @@ class MainActivity : AppCompatActivity() {
         setupBottomNavMenu(navController)
         setupSideNavigationMenu(navController)
         setupActionBar(navController)
-
-        if (savedInstanceState == null) {
-            /*supportFragmentManager.beginTransaction()
-                    .setCustomAnimations(R.anim.fragment_close_enter, R.anim.fragment_fade_exit,
-                                R.anim.fragment_fast_out_extra_slow_in, R.anim.fragment_open_enter)
-                    .add(R.id.nav_host_fragment, LoginFragment.newInstance())
-                    .commitNow()*/
-        }
     }
 
     private fun setupBottomNavMenu(navController: NavController) {
@@ -53,7 +42,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupActionBar(navController: NavController) {
         appBarConfiguration = AppBarConfiguration(navController.graph)
-        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
+        NavigationUI.setupActionBarWithNavController(this, navController, container)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -70,6 +59,6 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
         appBarConfiguration = AppBarConfiguration(navController.graph)
-        return NavigationUI.navigateUp(navController, drawer_layout)
+        return NavigationUI.navigateUp(navController, container)
     }
 }
